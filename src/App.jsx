f9eb937a4c563562d8c65cd5f90bcef9ce91d75e
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-01-26 23:24:37 +0100

    load facted image from 2nd API

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 
 function App() {
   const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
     fetch('https://catfact.ninja/fact')
@@ -11,12 +12,24 @@ function App() {
       .then(({ fact }) => setFact(fact))
   }, [])
 
+  useEffect(() => {
+    if (!fact) return
+
+    const sayText = fact.split(/\s/g, 4).join(' ') + '...'
+    fetch(`https://cataas.com/cat/says/${sayText}?json=true`)
+      .then((response) => response.json())
+      .then(({ url }) => setImageUrl('https://cataas.com' + url))
+  }, [fact])
+
   return (
     <main className="App">
       <header>
         <h1>Cat Facts</h1>
       </header>
       {fact && <p>{fact}</p>}
+      {imageUrl && (
+        <img src={imageUrl} alt={`A fancy image with cats saying: ${fact}`} />
+      )}
     </main>
   )
 }
