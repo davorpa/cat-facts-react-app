801fb42e358b3d0a66848c2b792d47fd895fd07d
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-01-27 03:12:26 +0100

    manage loading and errors

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,42 @@ import './App.css'
 function App() {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
 
   useEffect(() => {
-    getNewRandomFact().then(setFact)
+    setLoading(true)
+    setError(null)
+    getNewRandomFact()
+      .then(setFact)
+      .catch(setError)
+      .finally(() => {
+        setLoading(false)
+      })
   }, [])
 
   useEffect(() => {
     if (!fact) return
-    getRandomImageUrlWith({ fact }).then(setImageUrl)
+    setLoading(true)
+    setError(null)
+    getRandomImageUrlWith({ fact })
+      .then(setImageUrl)
+      .catch(setError)
+      .finally(() => {
+        setLoading(false)
+      })
   }, [fact])
 
   const handleGetAnotherFactClick = (event) => {
-    getNewRandomFact().then(setFact)
+    if (loading) return
+    setLoading(true)
+    setError(null)
+    getNewRandomFact()
+      .then(setFact)
+      .catch(setError)
+      .finally(() => {
+        setLoading(false)
+      })
   }
 
   return (
@@ -26,11 +50,14 @@ function App() {
       <header>
         <h1>Cat Facts</h1>
       </header>
-      <button onClick={handleGetAnotherFactClick}>Get another fact</button>
+      <button disabled={loading} onClick={handleGetAnotherFactClick}>
+        Get another fact
+      </button>
       {fact && <p>{fact}</p>}
       {imageUrl && (
         <img src={imageUrl} alt={`A fancy image with cats saying: ${fact}`} />
       )}
+      {error && <p className="error">{error.message}</p>}
     </main>
   )
 }
